83f52b8d8bff739dba8ae645f0aae8d622dd710a
Micah Effiong <micaiah.effiong@gmail.com>
2024-01-15 10:56:26 +0100

    chore: initial commit

---
+++ android/src/main/java/com/rnpip/RnPipModule.java
@@ -0,0 +1,32 @@
+package com.rnpip;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = RnPipModule.NAME)
+public class RnPipModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "RnPip";
+
+  public RnPipModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
