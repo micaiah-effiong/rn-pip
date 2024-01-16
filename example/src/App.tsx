5f07ea984f847a7e76bc23348e144de588ea84eb
Micah Effiong <micaiah.effiong@gmail.com>
2024-01-16 13:50:44 +0100

    feat: enable pip-mode auto

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,42 +1,24 @@
 import * as React from 'react';
-import { useRef } from 'react';
 
-import { StyleSheet, View, Text, Button, AppState } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import {
   multiply,
   enterPictureInPictureMode,
   usePipModeListener,
+  enableAutoPipMode,
 } from 'rn-pip';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
-  const appState = useRef(AppState.currentState);
+  // const appState = useRef(AppState.currentState);
   const inPipMode = usePipModeListener();
 
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    multiply(3, 6).then(setResult);
   }, []);
 
   React.useEffect(() => {
-    const subscription = AppState.addEventListener('change', (nextAppState) => {
-      if (
-        appState.current.match(/inactive|background/) &&
-        nextAppState === 'active'
-      ) {
-        console.log('App came forward');
-      } else {
-        console.log('App went back');
-        /** activate pip */
-        enterPictureInPictureMode(300, 400);
-      }
-
-      appState.current = nextAppState;
-      console.log('AppState', appState.current);
-    });
-
-    return () => {
-      subscription.remove();
-    };
+    enableAutoPipMode(true);
   }, []);
 
   return (
