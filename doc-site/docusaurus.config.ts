df488dbc556f8ba46a4e2550db2499520e7bbc38
sammy-code98 <sammysmart98@yahoo.com>
2024-02-21 12:07:04 +0100

    fix: pull from feat/docs and remove blog folder

--- doc-site/docusaurus.config.ts
+++ doc-site/docusaurus.config.ts
@@ -157,7 +157,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Rn-pip. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
